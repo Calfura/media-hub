62e31e6d5206599a6318b737af9617d5ad05f3d7
Calfura <travis_wilson999@hotmail.com>
2024-05-29 17:45:54 +1000

    feat: Dropdown button implemented. Working on functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,23 @@
-
+import "../styles/Footer.css";
 
 export default function(){
     
     return (
         // General footer located at the bottem of the page.
         <footer>
-            <h6>Copyright 2024 Calfura</h6>
-            <a href="calfura@example-email.com">Email us!</a>
+
+            {/* Having a copyright and social media side section to the bottem left */}
+            <div id="socialMedia">
+                <h6>Copyright (C) 2024 Calfura</h6>
+                <h6><a href="calfura@example-email.com"><b>Contact us!</b></a></h6>
+            </div>
+
+            {/* Have fake app install buttons, that if worked would redirect to the app store to install */}
+            <div id="appInstall">
+
+            </div>
+
+
         </footer>
     )
 }
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Template />} >
-        <Route index element={<HomePage />} />
-      </Route>
-    </Routes>
+    <div className="App">
+      <Header >
+
+      </Header>
+      <Footer >
+
+      </Footer>
+      
+    </div>
   );
 }
 
